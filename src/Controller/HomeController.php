7660eb881763532b9f9cd4afd951cd67dc6e512e
Adrien Roches <rochesadrien@gmail.com>
2022-12-12 10:58:02 +0100

    [UPDATE] Move from Service\Movies to using the MovieRepository

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-use App\Service\Movies;
+use App\ReadModel\Movie;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'movies' => Movies::getMovies(),
+            'movies' => Movie::fromEntities($movieRepository->findAll()),
         ]);
     }
 }
--- src/ReadModel/Movie.php
+++ src/ReadModel/Movie.php
@@ -2,7 +2,9 @@
 
 namespace App\ReadModel;
 
+use App\Entity\Movie as MovieEntity;
 use DateTimeImmutable;
+use function array_map;
 
 class Movie
 {
@@ -15,5 +17,25 @@ class Movie
         public readonly array             $genres,
         public readonly string            $poster,
     ) {
+    }
+
+    /**
+     * @param array<int, MovieEntity> $movieEntities
+     *
+     * @return array<int, self>
+     */
+    public static function fromEntities(array $movieEntities): array
+    {
+        return array_map(self::fromEntity(...), $movieEntities);
+    }
+
+    public static function fromEntity(MovieEntity $movieEntity): self
+    {
+        return new self(
+            $movieEntity->getTitle(),
+            $movieEntity->getReleasedAt(),
+            [],
+            $movieEntity->getPoster(),
+        );
     }
 }
