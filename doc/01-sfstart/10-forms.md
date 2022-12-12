0b0c18f85825722afcc3011b2f6fabf634bc29a6
Adrien Roches <rochesadrien@gmail.com>
2022-12-12 11:50:36 +0100

    [FEATURE] Create a form type to add a movie + display it.

---
+++ doc/01-sfstart/10-forms.md
@@ -0,0 +1,13 @@
+Exercise 10 - Create and display a form
+=======================================
+
+1. Create a new form to add / edit a movie.
+   1. Update the movie controller
+   2. Create a new type
+   3. Display the form
+
+
+# Steps
+```shell
+$ symfony console make:form MovieType Movie
+```
--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\MovieType;
 use App\ReadModel\Movie;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,11 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movies/{slug<[a-zA-Z0-9-]+>}', name: 'movie_details', methods: ['GET'])]
+    #[Route('/movies/{slug<[a-zA-Z0-9-]+>}', name: 'movie_details', methods: ['GET'], priority: -100)]
     public function details(MovieRepository $movieRepository, string $slug): Response
     {
         return $this->render('movie/index.html.twig', [
             'movie' => Movie::fromEntity($movieRepository->fetchOneBySlug($slug)),
+        ]);
+    }
+
+    #[Route('/movies/add', name: 'movie_add', methods: ['GET', 'POST'])]
+    public function add(): Response
+    {
+        $form = $this->createForm(MovieType::class);
+
+        return $this->render('movie/add.html.twig', [
+            'form' => $form,
         ]);
     }
 }
