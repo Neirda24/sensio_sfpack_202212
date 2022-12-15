2cdf343883a8ce62a71167e3b89459a8e0de4f6b
Adrien Roches <rochesadrien@gmail.com>
2022-12-15 09:30:21 +0100

    [UPDATE] Add edit + delete on movies.

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Form\MovieType;
 use App\ReadModel\Movie;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\NoResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Movie as MovieEntity;
 
 class MovieController extends AbstractController
 {
@@ -21,18 +23,39 @@ class MovieController extends AbstractController
     }
 
     #[Route('/movies/add', name: 'movie_add', methods: ['GET', 'POST'])]
-    public function add(Request $request, MovieRepository $movieRepository): Response
+    #[Route('/movies/{slug<[a-zA-Z0-9-]+>}/edit', name: 'movie_edit', methods: ['GET', 'POST'])]
+    public function add(Request $request, MovieRepository $movieRepository, ?string $slug): Response
     {
-        $form = $this->createForm(MovieType::class);
+        $movie = new MovieEntity();
+
+        if (null !== $slug) {
+            $movie = $movieRepository->fetchOneBySlug($slug);
+        }
+
+        $form = $this->createForm(MovieType::class, $movie);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $movieRepository->save($form->getData(), true);
+            $movieRepository->save($movie, true);
 
             return $this->redirectToRoute('app_home');
         }
 
         return $this->render('movie/add.html.twig', ['form' => $form]);
+    }
+
+    #[Route('/movies/{slug<[a-zA-Z0-9-]+>}/delete', name: 'movie_delete', methods: ['GET'])]
+    public function delete(Request $request, MovieRepository $movieRepository, string $slug): Response
+    {
+        try {
+            $movie = $movieRepository->fetchOneBySlug($slug);
+        } catch (NoResultException) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $movieRepository->remove($movie, true);
+
+        return $this->redirectToRoute('app_home');
     }
 }
