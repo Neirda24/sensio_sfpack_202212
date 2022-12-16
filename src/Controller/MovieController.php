4040fd4da364734733503388739047fedd2cc9a3
Adrien Roches <rochesadrien@gmail.com>
2022-12-16 11:34:16 +0100

    [UPDATE] Secure the DELETE + change wording if unauthorized to create a movie + secure the fixtures with clock

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Throwable;
 
@@ -69,6 +70,7 @@ class MovieController extends AbstractController
     }
 
     #[Route('/movies/{slug<[a-zA-Z0-9-]+>}/delete', name: 'movie_delete', methods: ['GET'])]
+    #[IsGranted('ROLE_ADMIN')]
     public function delete(string $slug): Response
     {
         try {
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,25 +6,29 @@ use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Clock\ClockInterface;
 use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class UserFixtures extends Fixture
 {
     private const USERS = [
         'child' => [
-            'dateOfBirth' => '07/04/2015',
+            'dateOfBirth' => '07/04',
+            'age'         => 7,
             'email'       => 'louise@example.com',
             'password'    => 'louise',
             'admin'       => false,
         ],
         'teen'  => [
-            'dateOfBirth' => '19/07/2003',
+            'dateOfBirth' => '19/07',
+            'age'         => 15,
             'email'       => 'max@example.com',
             'password'    => 'max',
             'admin'       => false,
         ],
         'adult' => [
-            'dateOfBirth' => '11/09/1990',
+            'dateOfBirth' => '11/09',
+            'age'         => 32,
             'email'       => 'adrien@example.com',
             'password'    => 'adrien',
             'admin'       => true,
@@ -33,6 +37,7 @@ class UserFixtures extends Fixture
 
     public function __construct(
         private readonly PasswordHasherFactoryInterface $passwordHasherFactory,
+        private readonly ClockInterface                 $clock,
     ) {
     }
 
@@ -41,8 +46,12 @@ class UserFixtures extends Fixture
         foreach (self::USERS as $userDetails) {
             $user = (new User())
                 ->setEmail($userDetails['email'])
-                ->setDateOfBirth(DateTimeImmutable::createFromFormat('d/m/Y', $userDetails['dateOfBirth']))
-            ;
+                ->setDateOfBirth(
+                    DateTimeImmutable::createFromFormat(
+                        'd/m/Y',
+                        $userDetails['dateOfBirth'] . '/' . $this->clock->now()->modify("-{$userDetails['age']} years")->format('Y'),
+                    ),
+                );
             $user->setPassword($this->passwordHasherFactory->getPasswordHasher($user)->hash($userDetails['password']));
 
             if (true === $userDetails['admin']) {
